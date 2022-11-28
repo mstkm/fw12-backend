59da57206a80a822117be995e9fa4eb0bf830057
mstkm <99125333+mstkm@users.noreply.github.com>
2022-11-28 16:39:14 +0700

    add auth login and register controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,38 @@
+const {readUserByEmail, createUserModel} = require('../models/users.model')
+const jwt = require('jsonwebtoken')
+const errorHandler = require('../helpers/errorHandler.helper')
+
+exports.login = (req, res) => {
+  readUserByEmail(req.body.email, (err, {rows}) => {
+    if(rows.length) {
+      const [user] = rows;
+      if (req.body.password === user.password) {
+        const token = jwt.sign({id: user.id}, 'backend-secret')
+        return res.status(200).json({
+          success: true,
+          message: 'Login success',
+          results: {
+            token
+          }
+        })
+      }
+    }
+    return res.status(401).json({
+      success: false,
+      message: 'Wrong email or password'
+    })
+  })
+}
+
+exports.register = (req, res) => {
+  createUserModel(req.body, (err, data) => {
+    if (err) {
+      return errorHandler(err, res);
+    }
+    return res.status(200).json({
+      success: true,
+      message: 'Register success',
+      results: data.rows[0]
+    })
+  })
+}
