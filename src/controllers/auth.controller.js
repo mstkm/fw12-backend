18b5063699afb9c0750dd4e969fc796422142bf3
mstkm <99125333+mstkm@users.noreply.github.com>
2023-01-12 08:57:19 +0700

    add results register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -67,9 +67,20 @@ exports.register = (req, res) => {
     if (err) {
       return errorHandler(err, res);
     }
-    return res.status(200).json({
-      success: true,
-      message: 'Register success'
+    readUserByEmail(req.body.email, (err, {rows}) => {
+      if(rows.length) {
+        const [user] = rows;
+        if (req.body.password === user.password) {
+          const token = jwt.sign({id: user.id}, 'backend-secret')
+          return res.status(200).json({
+            success: true,
+            message: 'Register success',
+            results: {
+              token
+            }
+          })
+        }
+      }
     })
   })
 }
