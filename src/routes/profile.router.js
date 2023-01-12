a2bb7b9838b2a7bcc451ac2e4531f5b48600e963
mstkm <99125333+mstkm@users.noreply.github.com>
2023-01-12 08:59:30 +0700

    add router get transaction by id

--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -1,10 +1,10 @@
 const profileRouter = require('express').Router()
-const { readProfile, updateProfile, createTransaction } = require('../controllers/profile.controller')
+const { readProfile, updateProfile, createTransaction, readTransactionById } = require('../controllers/profile.controller')
 const uploadMiddleware = require('../middleware/upload.middleware')
-
 
 profileRouter.get('/', readProfile)
 profileRouter.patch('/', uploadMiddleware, updateProfile)
 profileRouter.post('/', createTransaction)
+profileRouter.get('/transaction/details/:id', readTransactionById)
 
 module.exports = profileRouter
