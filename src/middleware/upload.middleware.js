3f1f30d26346b9247cc915c067ea1f8c1cd9b882
mstkm <99125333+mstkm@users.noreply.github.com>
2023-01-15 15:15:48 +0700

    implement cloudinary storage

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -1,25 +1,48 @@
-const multer = require('multer');
-const errorHandler = require('../helpers/errorHandler.helper');
+const multer = require('multer')
+const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
+const errorHandler = require('../helpers/errorHandler.helper')
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/')
-  },
-  filename: (req, file, cb) => {
-    const extension = file.originalname.split('.');
-    const ext = extension[extension.length-1];
-    const name = `${new Date().getDate()}_${new Date().getTime()}.${ext}`;
-    cb(null, name)
-  }
+// Configuration
+cloudinary.config({
+  cloud_name: 'dvzrmzldr',
+  api_key: '482277453968621',
+  api_secret: 'lGk-vJmdwpUfXjNPs0BByUfNuRc'
 })
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads/')
+//   },
+//   filename: (req, file, cb) => {
+//     const extension = file.originalname.split('.')
+//     const ext = extension[extension.length - 1]
+//     const name = `${new Date().getDate()}_${new Date().getTime()}.${ext}`
+//     cb(null, name)
+//   }
+// })
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'karcis',
+    format: async (req, file) => file.originalname.split('.')[file.originalname.split('.').length - 1], // supports promises as well
+    public_id: (req, file) => {
+      const extension = file.originalname.split('.')
+      const ext = extension[extension.length - 1]
+      const name = `${new Date().getDate()}_${new Date().getTime()}.${ext}`
+      return name
+    },
+  },
+});
 
 const fileFilter = (req, file, cb) => {
   console.log(file)
-  if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
-    cb(null, true);
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true)
   } else {
-    cb(null, false);
-    return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    cb(null, false)
+    return cb(new Error('Only .png, .jpg and .jpeg format allowed!'))
   }
 }
 
@@ -27,7 +50,7 @@ const limits = {
   fileSize: 5024 * 5024
 }
 
-const upload =  multer({
+const upload = multer({
   storage,
   fileFilter,
   limits
@@ -39,7 +62,7 @@ module.exports = (req, res, next) => {
   uploadMiddleware(req, res, (err) => {
     if (err) {
       console.log(err)
-      return errorHandler(err, res);
+      return errorHandler(err, res)
     }
     next()
   })
